5e80cbdaf1d43b1524dd158d3fc4a95dc5950e42
Joshua <merkleyjosh@gmail.com>
2017-11-17 00:00:24 +0000

    Commit with backend mongoDB

--- monCR/routes/index.js
+++ monCR/routes/index.js
@@ -1,9 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+var mongoose = require('mongoose'); 
+
+mongoose.connect('mongodb://localhost/taskDB', { useMongoClient: true }); 
+
+var taskSchema = mongoose.Schema({ 
+Task: String
+});
+
+var Task = mongoose.model('Task', taskSchema); 
+
+var db = mongoose.connection; 
+db.on('error', console.error.bind(console, 'connection error:')); 
+db.once('open', function() { 
+console.log('Connected');
+});
+
+router.post('/comment', function(req, res, next) {
+var newtask = new Task(req.body); 
+console.log(newtask); 
+newtask.save(function(err, post) { 
+  if (err) return console.error(err);
+  console.log(post);
+  res.sendStatus(200);
+});
+});
+
+
+router.get('/comment', function(req, res, next) {
+console.log("In the GET route");
+Task.find(function(err,taskList) { 
+  if (err) return console.error(err); 
+  else {
+    console.log(taskList); 
+   res.json(taskList);  
+    
+  }
+})
+});
+
+router.delete('/comment', function(req,res) {
+console.log("deleting");
+Task.find();
+Task.remove(function(err,taskList){
+if (err) return console.error(err);
+else {
+console.log(taskList);
+}
+})
 });
 
 module.exports = router;
